110c04732812e2ea263d3cadfbeb781dadee8280
Jinusc <33141333+Jinusc@users.noreply.github.com>
2017-11-30 18:58:43 -0800

    Update main.go

--- service/main.go
+++ service/main.go
@@ -10,7 +10,9 @@ import (
 	"reflect"
 	"github.com/pborman/uuid"
 	"context"
-	"cloud.google.com/go/bigtable"
+	//"cloud.google.com/go/bigtable"
+	"cloud.google.com/go/storage"
+	"io"
 )
 
 const (
@@ -21,7 +23,8 @@ const (
 	PROJECT_ID = "newaround-186823"
 	BT_INSTANCE = "newaround-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.50.206 :9200"
+	ES_URL = "http://35.227.62.174:9200"
+	BUCKET_NAME = "post-images-iris"
 )
 
 type Location struct {
@@ -34,6 +37,7 @@ type Post struct {
 	User     string `json:"user"`
 	Message  string  `json:"message"`
 	Location Location `json:"location"`
+	Url    string `json:"url"`
 }
 
 func main() {
@@ -79,6 +83,7 @@ func main() {
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
+	/*
 	fmt.Println("Received one post request")
 	decoder := json.NewDecoder(r.Body)
 	var p Post
@@ -86,12 +91,92 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 		return
 	}
+	*/
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+
+	// 32 << 20 is the maxMemory param for ParseMultipartForm, equals to 32MB (1MB = 1024 * 1024 bytes = 2^20 bytes)
+	// After you call ParseMultipartForm, the file will be saved in the server memory with maxMemory size.
+	// If the file size is larger than maxMemory, the rest of the data will be saved in a system temporary file.
+	r.ParseMultipartForm(32 << 20)
+	// Parse from form data.
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
 	id := uuid.New()
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Image is not available", http.StatusInternalServerError)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
+	}
+	ctx := context.Background()
+	defer file.Close()
+
+	// replace it with your real bucket name.
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
+		return
+	}
+
+	// Update the media link after saving to GCS.
+	p.Url = attrs.MediaLink
+
+
 	// Save to ES.
-	saveToES(&p, id)
-	saveToBT(&p, id);
+	//saveToES(&p, id)
+	saveToES(p, id)
+	//saveToBT(&p, id);
 
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
+
+
+func saveToGCS(ctx context.Context, r io.Reader, bucket, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	// Student questions
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer client.Close()
+
+	bh := client.Bucket(bucket)
+	// Next check if the bucket exists
+	if _, err = bh.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+
+	obj := bh.Object(name)
+	w := obj.NewWriter(ctx)
+	if _, err := io.Copy(w, r); err != nil {
+		return nil, nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, nil, err
+	}
+
+
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
+
 }
 
 // Save a post to ElasticSearch
@@ -120,6 +205,7 @@ func saveToES(p *Post, id string) {
 }
 
 // Save a post to BT
+/*
 func saveToBT(p *Post, id string){
 	ctx := context.Background()
 	// you must update project name here
@@ -148,7 +234,7 @@ func saveToBT(p *Post, id string){
 
 
 }
-
+*/
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
